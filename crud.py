98eb8edccd5a7505d7f1147bd1d039e56a679ca2
benjcrowley <76239734+benjcrowley@users.noreply.github.com>
2023-01-03 00:11:29 -0700

    added functionality for users to rate movies, and then delete thier rating if they need to

--- crud.py
+++ crud.py
@@ -20,6 +20,12 @@ def get_user_by_id(user_id):
     """Return a user by id."""
     
     return User.query.get(user_id)
+# get user by email
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
+
 
 # create a Movie function
 def create_movie(title, overview, release_date, poster_path):
@@ -51,8 +57,21 @@ def create_rating(score, user, movie):
     rating = Rating(score=score, user=user, movie=movie)
 
     return rating
+# view all ratings
+def view_all_ratings(movie_id):
+    """View all ratings"""
 
+    return Rating.query.filter(Rating.movie_id == movie_id).all()
 
+def get_user_rating(user_id, movie_id):
+    """Get user rating"""
+
+    return Rating.query.filter(Rating.user_id == user_id, Rating.movie_id == movie_id).first()
+
+def get_rating_by_id(rating_id):
+    """Get rating by id"""
+
+    return Rating.query.get(rating_id)
 if __name__ == "__main__":
     from server import app
 
--- server.py
+++ server.py
@@ -29,10 +29,50 @@ def show_movie(movie_id):
     """Show details on a specific movie."""
 
     movie = crud.get_movie_by_id(movie_id)
+    all_ratings = crud.view_all_ratings(movie_id)
+    # get the rating from the current user in session
+    current_user_rating = None
+    if "user_id" in session:
+        current_user_rating = crud.get_user_rating(session["user_id"], movie_id)
 
-    return render_template("movie_details.html", movie=movie)
+    # get the average score from all ratings
+    total_score = 0
+    for rating in all_ratings:
+        total_score += rating.score
+    if len(all_ratings) > 0:
+        average_score = total_score / len(all_ratings)
+    print(all_ratings)
+    return render_template("movie_details.html", movie=movie, all_ratings=all_ratings, average_score=average_score, current_user_rating=current_user_rating)
 
+# Create a new rating for a movie
+@app.route("/movies/<movie_id>", methods=["POST"])
+def rate_movie(movie_id):
+    """Create a new rating for a movie."""
 
+    score = request.form.get("score")
+    user_id = session["user_id"]
+    movie = crud.get_movie_by_id(movie_id)
+    user = crud.get_user_by_id(user_id)
+
+    rating = crud.create_rating(score, user, movie)
+    db.session.add(rating)
+    db.session.commit()
+    flash("Rating added!")
+
+    return redirect(f"/movies/{movie_id}")
+# delete a rating
+@app.route("/remove_rating/<rating_id>/<movie_id>")
+def delete_rating(rating_id, movie_id):
+    """Delete a rating."""
+
+    rating = crud.get_rating_by_id(rating_id)
+    movie_id = movie_id
+    db.session.delete(rating)
+    db.session.commit()
+    flash("Rating deleted!")
+
+    return redirect(f"/movies/{movie_id}")
+    
 @app.route("/users")
 def all_users():
     """View all users."""
@@ -48,6 +88,45 @@ def show_user(user_id):
     user = crud.get_user_by_id(user_id)
 
     return render_template("user_profile.html", user=user)
+
+# Create a new user
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Create a new user."""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+
+    if user:
+        flash("This email is already registered.")
+    else:
+        user = crud.create_user(email, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created! Please log in.")
+
+    return redirect("/")
+
+# log in
+@app.route("/login" , methods=["POST"])
+def login():
+    """Log in user."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+    if user:
+        if user.password == password:
+            session["user_id"] = user.user_id
+            flash("Logged in!")
+        else:
+            flash("Incorrect password.")
+    else:
+        flash("No account with that email. Please register.")
+    
+    return redirect("/")
 
 if __name__ == "__main__":
     connect_to_db(app)
